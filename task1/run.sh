4ddc2a36724ee2f7405dbe3361b7d2e8f9df48f5
YuzhengCai <yuzhengcai21@m.fudan.edu.cn>
2024-06-15 06:19:54 +0000

    hyper parameter grid search for task1

--- task1/run.sh
+++ task1/run.sh
@@ -3,8 +3,14 @@
 # specify the path to the checkpoint directory
 export TORCH_HOME="checkpoint/SupervisedPretrained"
 
-# test supervised pretrained model
-nohup python train.py --model_type SupervisedPretrained --device cuda:1 > test_supervised_pretrained.txt 2>&1 &
+# run supervised pretrained model
+nohup python train.py --model-type SupervisedPretrained --device cuda:1 > run_supervised_pretrained.txt 2>&1 &
 
-# test supervised model
-nohup python train.py --model_type Supervised --device cuda:1 > test_supervised.txt 2>&1 &
+# run supervised model
+nohup python train.py --model-type Supervised --device cuda:1 > run_supervised.txt 2>&1 &
+
+# run grid search for supervised pretrained model
+nohup bash supervised_pretrained_grid_search.sh > supervised_pretrained_grid_search.txt 2>&1 &
+
+# run grid search for supervised model
+nohup bash supervised_grid_search.sh > supervised_grid_search.txt 2>&1 &
---
+++ task1/supervised_pretrained_grid_search.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# parameters for grid search
+lrs=(0.001 0.01 0.1)
+optimizers=("adam" "sgd")
+batch_sizes=(32 64 128 256)
+
+# run grid search
+for lr in "${lrs[@]}"; do
+    for optimizer in "${optimizers[@]}"; do
+        for batch_size in "${batch_sizes[@]}"; do
+            echo "-------------------------------------------------------"
+            echo "lr: $lr, optimizer: $optimizer, batch_size: $batch_size"
+            python train.py \
+                --model-type "SupervisedPretrained" \
+                --lr "$lr" \
+                --optimizer "$optimizer" \
+                --batch-size "$batch_size" \
+                --data "cifar100" \
+                --device "cuda:3"
+        done
+    done
+done
